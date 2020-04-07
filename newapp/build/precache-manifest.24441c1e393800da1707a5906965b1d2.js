1d7e80e1a51a9a1f2873d088dcd23f77a00ad243
terrenceoconnor57 <60070754+terrenceoconnor57@users.noreply.github.com>
2020-04-07 09:05:05 -0400

    npm run build

---
+++ newapp/build/precache-manifest.24441c1e393800da1707a5906965b1d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60834d2679fb002def70107e99c1436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d104be6c8f6bcdf61a1d",
+    "url": "/static/css/main.7e4f7f69.chunk.css"
+  },
+  {
+    "revision": "d1a7391ae9b113716790",
+    "url": "/static/js/2.4a509bd9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.4a509bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d104be6c8f6bcdf61a1d",
+    "url": "/static/js/main.083e0182.chunk.js"
+  },
+  {
+    "revision": "1741d81e4cfcf761b2c7",
+    "url": "/static/js/runtime-main.127a0dcf.js"
+  },
+  {
+    "revision": "dbd9fc1f19ab3bb7d547f910104b5951",
+    "url": "/static/media/blackandwhite.dbd9fc1f.jpeg"
+  }
+]);
